95ab8290133da23876326d19447efea0676f2e49
spencerprescott <stp@umail.ucsb.edu>
2016-05-21 22:26:11 -0700

    Add port to server

--- app.py
+++ app.py
@@ -2,15 +2,12 @@ import sys
 from HttpServer import *
 from Routing import Router
 
-try:
-	port_number = sys.argv[1]
-	print port_number
-except:
-	print "Error: Port number not included in argument list. Please run app as 'python app.py [PORT]'"
-
-
 if __name__ == '__main__':
-	router = Router()
-	router.addRoute("names", "NamesController#index")
-	router.addRoute("sort/*", "SortController#sort")
-	HttpServer(router).listen(8000)
+	try:
+		port_number = sys.argv[1]
+		router = Router()
+		router.addRoute("names", "NamesController#index")
+		router.addRoute("sort/*", "SortController#sort")
+		HttpServer(router).listen(int(port_number))
+	except:
+		print "Error: Port number not included in argument list. Please run app as 'python app.py [PORT]'"
